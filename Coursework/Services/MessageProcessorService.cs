a784d74d598d4219b0662bb03bf4baff815ea33a
SachaMillerDev <sachamiller.hi@gmail.com>
2023-08-17 09:42:08 +0100

    update

--- Coursework/Services/MessageProcessorService.cs
+++ Coursework/Services/MessageProcessorService.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Coursework.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Coursework.Services
 {
-    internal class MessageProcessorService
+    public class MessageProcessorService
     {
+        public MessageBase ProcessMessage(string messageId, string body)
+        {
+            switch (messageId[0])
+            {
+                case 'S':
+                    // Process SMS message
+                    return ProcessSMSMessage(messageId, body);
+                case 'E':
+                    // Process Email message
+                    return ProcessEmailMessage(messageId, body);
+                case 'T':
+                    // Process Tweet
+                    return ProcessTweetMessage(messageId, body);
+                default:
+                    throw new ArgumentException("Invalid message type");
+            }
+        }
+
+        private SMSMessage ProcessSMSMessage(string messageId, string body)
+        {
+            // Extract sender and message text from body
+            // Return new SMSMessage object
+        }
+
+        private EmailMessage ProcessEmailMessage(string messageId, string body)
+        {
+            // Extract sender, subject, and message text from body
+            // Return new EmailMessage object
+        }
+
+        private TweetMessage ProcessTweetMessage(string messageId, string body)
+        {
+            // Extract Twitter ID and tweet text from body
+            // Return new TweetMessage object
+        }
     }
+
 }
